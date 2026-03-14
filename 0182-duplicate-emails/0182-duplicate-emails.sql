da78654f453436c70f85a7771c49b106cfbfbdee
muhammad abdullah <abdullahkhosa11223344@gmail.com>
2026-03-14 02:29:01 -0700

    Time: 350 ms (76.14%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,4 +1,8 @@
-# Write your MySQL query statement below
+# Write a solution to report all the duplicate emails
+#Note that it's guaranteed that the email field is not NULL.
+
+# Return the result table in any order.
+
 Select
     email
 from 
