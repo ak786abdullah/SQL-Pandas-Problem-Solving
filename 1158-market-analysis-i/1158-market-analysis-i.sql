7c352b72b091c9e70a5872f0416ee4d87851448f
muhammad abdullah <abdullahkhosa11223344@gmail.com>
2026-03-14 10:51:27 -0700

    Time: 1075 ms (94.91%), Space: 0B (100%) - LeetHub

---
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -0,0 +1,11 @@
+-- Write a solution to find for each user, the join date and the number of orders they made as a buyer in 2019.
+
+-- Return the result table in any order.
+SELECT
+    u.user_id as buyer_id,
+    u.join_date,
+    IFNULL(COUNT(o.buyer_id),0) as orders_in_2019
+FROM
+    Users u LEFT JOIN Orders o ON u.user_id=o.buyer_id AND YEAR(order_date)=2019
+GROUP BY
+    u.user_id;
