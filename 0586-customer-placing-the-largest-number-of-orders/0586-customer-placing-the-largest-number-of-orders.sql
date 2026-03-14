43c947f048f247e8d6a25f796c4e976dfe31fd7b
muhammad abdullah <abdullahkhosa11223344@gmail.com>
2026-03-14 08:21:19 -0700

    Time: 412 ms (92.98%), Space: 0B (100%) - LeetHub

---
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -0,0 +1,15 @@
+-- Write a solution to find the customer_number for the 
+-- customer who has placed the largest number of orders.
+
+-- The test cases are generated so that exactly one 
+-- customer will have placed more orders than any other customer.
+
+SELECT 
+    customer_number 
+from
+    orders 
+group by 
+    customer_number 
+order by 
+    count(customer_number) DESC 
+LIMIT 1;
