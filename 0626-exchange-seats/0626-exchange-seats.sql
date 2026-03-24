02c9d5910c9fe2904d16a3088b02e5de28f5911c
muhammad abdullah <abdullahkhosa11223344@gmail.com>
2026-03-24 04:06:43 -0700

    Time: 490 ms (8.01%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -3,12 +3,21 @@
 -- Return the result table ordered by id in ascending order.
 
 SELECT 
-    CASE 
-        WHEN id % 2 = 1 AND id != (select max(id) from seat) THEN id + 1 
-        WHEN id % 2 =0 THEN id -1 
-        ELSE id 
-        END as id, student
-FROM
+    CASE
+        WHEN
+            id % 2=1 AND id != (
+            SELECT max(id) FROM Seat
+            )
+        THEN
+            id + 1 
+        WHEN 
+            id % 2 = 0 
+        THEN 
+            id-1
+    ELSE 
+        id
+    END AS id ,student
+FROM 
     Seat
-ORDER BY
-     id asc ;
+ORDER BY 
+    id;
