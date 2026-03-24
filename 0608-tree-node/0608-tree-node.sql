8f85608994033b56bdf8fc7f83a7f2b94ea2e88d
muhammad abdullah <abdullahkhosa11223344@gmail.com>
2026-03-24 04:07:25 -0700

    Time: 621 ms (9.26%), Space: 0B (100%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -10,8 +10,20 @@
 SELECT 
     id,
     CASE 
-        WHEN p_id is null THEN 'Root'
-        WHEN id in (select p_id from Tree) THEN 'Inner'
-        ELSE 'Leaf'
-    END as type
-FROM Tree;
+        WHEN 
+            p_id IS NULL 
+        THEN 
+            'Root'
+        WHEN id IN (
+            SELECT 
+                p_id 
+            FROM 
+                Tree
+        )
+        THEN  
+            'Inner'
+        ELSE 
+            'Leaf' 
+        END AS type 
+FROM 
+    Tree
