19ffcaabd2b38b300d75d5e0002d31fd41a55b4e
muhammad abdullah <abdullahkhosa11223344@gmail.com>
2026-03-24 09:03:02 -0700

    Time: 377 ms (20.12%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,13 +1,14 @@
 -- Write a solution to find managers with at least five direct reports.
 
 -- Return the result table in any order.
-SELECT
+SELECT 
     m.name
 FROM 
-    Employee m
-    join Employee e
-    on m.id=e.managerId
+    Employee m 
+    join 
+    Employee e
+    on m.id=e.managerId 
 GROUP BY 
-    e.managerId,m.name 
+    m.id,m.name 
 HAVING 
-    COUNT(e.managerId) >=5;
+    COUNT(m.id)>=5;
