3e4955d6b66d728366a6a169b1673611a6369c77
muhammad abdullah <abdullahkhosa11223344@gmail.com>
2026-03-24 04:03:19 -0700

    Time: 454 ms (67.58%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -3,29 +3,32 @@
 -- Write a solution to find the prices of all products on the date 2019-08-16.
 
 -- Return the result table in any order.
+WITH RankPrices as(
+    SELECT 
+        product_id,
+        new_price,
+        dense_rank() over (partition by product_id order by change_date desc) as rn 
+    FROM 
+        Products 
+    WHERE 
+        change_date <= '2019-08-16'
+)
 SELECT 
-    product_id,new_price as price
+    product_id,
+    new_price as price 
 FROM 
-    Products
+    RankPrices
 WHERE 
-    (product_id,change_date) IN (
-        SELECT 
-            product_id,MAX(change_date)
-        FROM 
-            Products
-        WHERE 
-            change_date <= '2019-08-16' 
-        GROUP BY 
-            product_id
-    ) 
+    rn =1
 
 UNION ALL 
 
-SELECT
-    product_id,10 as price
+SELECT 
+    product_id ,
+    10 as price
 FROM 
-    Products
+    Products 
 GROUP BY 
-    product_id
-HAVING
-    MIN(change_date) > '2019-08-16'
+    product_id 
+HAVING 
+    MIN(change_date) > '2019-08-16';
