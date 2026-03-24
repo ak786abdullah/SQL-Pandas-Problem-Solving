1661260ff577b18101959a7fe5573cdaafe36b2e
muhammad abdullah <abdullahkhosa11223344@gmail.com>
2026-03-24 13:10:13 -0700

    Time: 326 ms (33.49%), Space: 0B (100%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -4,10 +4,8 @@
 -- If there is a tie between two scores, both should have the same ranking.
 -- After a tie, the next ranking number should be the next consecutive integer value. In other words, there should be no holes between ranks.
 -- Return the result table ordered by score in descending order.
-SELECT
+SELECT 
     score,
-    dense_rank() over (order by score desc) as 'rank'
-from 
-    scores 
-order by 
-    score desc;
+    DENSE_RANK() OVER (ORDER BY score DESC) AS 'rank'
+FROM
+    Scores
