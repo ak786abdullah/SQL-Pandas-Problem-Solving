76639b1f15f0ae4d2760c74b32ee1121a6045cf2
muhammad abdullah <abdullahkhosa11223344@gmail.com>
2026-03-24 11:15:50 -0700

    Time: 597 ms (86.56%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,24 +1,23 @@
 -- Write a solution to find employees who have the highest salary in each of the departments.
 
 -- Return the result table in any order.
-with ranksalaries as (
+WITH SalaryRank AS (
+    SELECT 
+        e.name as Employee,
+        e.salary as Salary,
+        d.name as Department,
+        dense_rank() OVER (PARTITION BY d.name ORDER BY e.salary desc) as salary_rank
+    FROM 
+        Employee e 
+        join Department d
+        ON e.departmentId=d.id
+)
 SELECT 
-    d.name as Department,
-    e.name as Employee,
-    e.salary as Salary,
-    dense_rank() over (partition by d.name order by e.salary desc ) as salary_rank
-from
-    Department as d 
-    join Employee as e 
-    on d.id = e.departmentId 
-    ) 
-
-select
     Department,
     Employee,
-    salary 
-from 
-    ranksalaries 
-where 
-    salary_rank =1 ;
+    Salary
+FROM 
+    SalaryRank
+WHERE 
+    salary_rank =1;
 
