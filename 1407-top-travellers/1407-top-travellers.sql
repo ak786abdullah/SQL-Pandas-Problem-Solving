6cb47f8ef846d4baa9a21a00898eff103adec5ed
muhammad abdullah <abdullahkhosa11223344@gmail.com>
2026-03-25 04:24:24 -0700

    Time: 760 ms (82.55%), Space: 0B (100%) - LeetHub

--- 1407-top-travellers/1407-top-travellers.sql
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -1,14 +1,14 @@
 -- Write a solution to report the distance traveled by each user.
 
 -- Return the result table ordered by travelled_distance in descending order, if two or more users traveled the same distance, order them by their name in ascending order.
-select
-    u.name ,
-    ifnull(sum(distance) , 0) as travelled_distance 
-from 
-    users u 
-    left join rides r 
-    on u.id=r.user_id 
-group by 
-    u.id
-order by 
-    travelled_distance desc ,name asc ;
+SELECT
+    u.name,
+    IFNULL(SUM(r.distance),0) as travelled_distance
+FROM 
+    Users u
+    LEFT JOIN Rides r 
+    on u.id=r.user_id
+GROUP BY 
+    u.id,u.name
+ORDER BY 
+    travelled_distance DESC,u.name ASC;
