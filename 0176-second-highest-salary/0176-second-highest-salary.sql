a41644810649ceccac9804cdd5b2ee13e398e221
muhammad abdullah <abdullahkhosa11223344@gmail.com>
2026-03-25 03:03:24 -0700

    Time: 260 ms (67.52%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,15 +1,15 @@
 -- Write a solution to find the second highest distinct salary from the Employee table. If there is no second highest salary, return null (return None in Pandas).
-with salaries_rank as (
+WITH salary_ranks AS (
+    SELECT 
+        id,
+        salary,
+        DENSE_RANK() OVER (ORDER BY salary DESC) AS salary_rank
+    FROM
+        Employee 
+)
 SELECT
-    id,
-    salary,
-    dense_rank() over (order by salary desc) as ranks 
-from 
-    employee
-) 
-select 
-    max(salary) as SecondHighestSalary
-from 
-    salaries_rank
-where
-    ranks=2 ;
+    MAX(salary) AS SecondHighestSalary 
+FROM 
+    salary_ranks
+WHERE 
+    salary_rank =2 ; 
