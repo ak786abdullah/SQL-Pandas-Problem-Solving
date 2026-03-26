2b3078a8800452445d5680f12d2726fac904ae69
muhammad abdullah <abdullahkhosa11223344@gmail.com>
2026-03-25 23:52:54 -0700

    Time: 672 ms (73.8%), Space: 0B (100%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -1,18 +1,17 @@
 -- Write a solution to get the names of products that have at least 100 units ordered in February 2020 and their amount.
 
 -- Return the result table in any order.
+
 SELECT 
-    p.product_name ,
-    sum(o.unit) as unit
-from
-    products p
-join
-    orders o 
-on 
-    p.product_id = o.product_id 
+    p.product_name,
+    sum(o.unit) AS unit
+FROM 
+    Products p
+    join Orders o
+    on p.product_id = o.product_id
 WHERE 
-    o.order_date >= '2020-02-01' AND o.order_date < '2020-03-01'
-group by 
-    p.product_name 
-having 
-    unit >= 100 ;
+    o.order_date >= '2020-02-01' AND o.order_date <= '2020-02-29'
+GROUP BY
+    p.product_id
+HAVING 
+    sum(o.unit) >= 100
