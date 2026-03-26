76575ef012eec823753693b66c05a96ae356fb33
muhammad abdullah <abdullahkhosa11223344@gmail.com>
2026-03-26 00:31:45 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -1,13 +1,14 @@
 -- Write a solution to find the average selling price for each product. average_price should be rounded to 2 decimal places. If a product does not have any sold units, its average selling price is assumed to be 0.
 
 -- Return the result table in any order.
+
 SELECT
-    P.product_id,
-    ifnull(round(sum(price * units) / sum(units),2),0) as average_price
+    p.product_id,
+    ROUND(SUM(p.price * u.units)/ SUM(u.units),2) average_price
 FROM
-    prices p 
-    left join UnitsSold u
-    on p.product_id=u.product_id
-    and u.purchase_date between p.start_date and p.end_date
-GROUP BY
-    p.product_id ;
+    Prices AS p
+    LEFT JOIN UnitsSold AS u
+    ON p.product_id=u.product_id
+    AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY 
+    p.product_id
