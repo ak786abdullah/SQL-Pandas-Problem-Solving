f0c41c340a008fea29152959e2b4dd2e6e96d478
muhammad abdullah <abdullahkhosa11223344@gmail.com>
2026-03-30 21:29:13 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,18 +1,18 @@
 # Write your MySQL query statement below
 
 # Find the percentage of 1st order that are immidiate
-WITH order_ranks AS (
-    SELECT
+WITH order_rank AS (
+    SELECT 
         customer_id,
         order_date,
-        customer_pref_delivery_date AS pref_date, 
-        DENSE_RANK() OVER (PARTITION BY customer_id ORDER BY order_date) as order_rank
+        customer_pref_delivery_date,
+        DENSE_RANK() OVER (PARTITION BY customer_id ORDER BY order_date) as rn
     FROM 
-        Delivery
-) 
+        delivery
+)
 SELECT 
-   ROUND(SUM(CASE WHEN order_date = pref_date THEN 1 ELSE 0 END )* 100 / COUNT(distinct customer_id),2) as immediate_percentage
+    ROUND(SUM(CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END) * 100 / COUNT(*)) AS immediate_percentage 
 FROM 
-    order_ranks
-WHERE
-    order_rank=1;
+    order_rank
+WHERE 
+    rn =1;
