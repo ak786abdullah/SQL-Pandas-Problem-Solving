e985f6aeec39773c5e9c86a804959bd6b7226a85
muhammad abdullah <abdullahkhosa11223344@gmail.com>
2026-04-05 00:58:37 -0700

    Time: 586 ms (60.55%), Space: 0B (100%) - LeetHub

--- 3521-find-product-recommendation-pairs/3521-find-product-recommendation-pairs.sql
+++ 3521-find-product-recommendation-pairs/3521-find-product-recommendation-pairs.sql
@@ -1,3 +1,4 @@
+# it is guarented that customer perchase different product
 SELECT 
     A.product_id AS product1_id,
     B.product_id AS product2_id,
@@ -6,15 +7,14 @@ SELECT
     COUNT(A.user_id) AS customer_count
 FROM 
     productpurchases A 
-    JOIN productpurchases B ON A.user_id=B.user_id
-    JOIN productInfo C ON A.product_id = C.product_id
-    JOIN productInfo D ON B.product_id = D.product_id 
+    JOIN productpurchases B ON A.user_id=B.user_id -- same customer with different product pairs
+    JOIN productInfo C ON A.product_id = C.product_id -- to find product1 category 
+    JOIN productInfo D ON B.product_id = D.product_id -- to find product2 category 
 WHERE 
-    B.product_id > A.product_id
+    B.product_id > A.product_id -- filter out duplicated pairs 
 GROUP BY 
     product1_id,product2_id 
 HAVING 
     customer_count >=3
 ORDER BY 
     customer_count DESC ,product1_id ASC ,product2_id ASC;
-
