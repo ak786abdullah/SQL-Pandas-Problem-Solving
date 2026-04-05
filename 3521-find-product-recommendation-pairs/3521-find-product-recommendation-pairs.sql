a8b1a5dceb76584e10aeed31e9bb3050b81fb791
muhammad abdullah <abdullahkhosa11223344@gmail.com>
2026-04-05 00:30:03 -0700

    Time: 682 ms (20.91%), Space: 0B (100%) - LeetHub

--- 3521-find-product-recommendation-pairs/3521-find-product-recommendation-pairs.sql
+++ 3521-find-product-recommendation-pairs/3521-find-product-recommendation-pairs.sql
@@ -1,19 +1,20 @@
 SELECT 
-    A.product_id AS product1_id, 
+    A.product_id AS product1_id,
     B.product_id AS product2_id,
     C.category AS product1_category,
-    D.category AS product2_category, 
+    D.category AS product2_category,
     COUNT(A.user_id) AS customer_count
 FROM 
     productpurchases A 
     JOIN productpurchases B ON A.user_id=B.user_id
-    JOIN ProductInfo C ON A.product_id=C.product_id
-    JOIN productInfo D ON B.product_id =D.product_id
+    JOIN productInfo C ON A.product_id = C.product_id
+    JOIN productInfo D ON B.product_id = D.product_id 
 WHERE 
     B.product_id > A.product_id
 GROUP BY 
-    product1_id,product2_id
+    product1_id,product2_id 
 HAVING 
-    customer_count >= 3
+    customer_count >=3
 ORDER BY 
-    customer_count DESC ,product1_id,product2_id;
+    customer_count DESC ,product1_id ASC ,product2_id ASC;
+
