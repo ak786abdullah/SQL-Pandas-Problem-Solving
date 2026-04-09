b7df1b72851d284926196252ff74818f34fb7f7f
muhammad abdullah <abdullahkhosa11223344@gmail.com>
2026-04-09 01:31:49 -0700

    Time: 699 ms (18.87%), Space: 0B (100%) - LeetHub

--- 3521-find-product-recommendation-pairs/3521-find-product-recommendation-pairs.sql
+++ 3521-find-product-recommendation-pairs/3521-find-product-recommendation-pairs.sql
@@ -7,14 +7,14 @@ SELECT
     COUNT(A.user_id) AS customer_count
 FROM 
     productpurchases A 
-    JOIN productpurchases B ON A.user_id=B.user_id -- same customer with different product pairs
-    JOIN productInfo C ON A.product_id = C.product_id -- to find product1 category 
-    JOIN productInfo D ON B.product_id = D.product_id -- to find product2 category 
+    JOIN productpurchases B ON A.user_id=B.user_id
+    JOIN productinfo C ON A.product_id=C.product_id
+    JOIN productinfo D ON B.product_id=D.product_id
 WHERE 
-    B.product_id > A.product_id -- filter out duplicated pairs 
+    B.product_id > A.product_id
 GROUP BY 
-    product1_id,product2_id 
+    A.product_id, B.product_id
 HAVING 
-    customer_count >=3
+    customer_count>=3
 ORDER BY 
-    customer_count DESC ,product1_id ASC ,product2_id ASC;
+    customer_count DESC ,product1_id ASC ,product2_id ASC ;
