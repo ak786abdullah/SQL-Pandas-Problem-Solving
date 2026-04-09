034f67c1d5e7c93320d5bff38fe33ae05c86f8d1
muhammad abdullah <abdullahkhosa11223344@gmail.com>
2026-04-09 00:43:30 -0700

    Time: 860 ms (5.5%), Space: 0B (100%) - LeetHub

--- 3580-find-consistently-improving-employees/3580-find-consistently-improving-employees.sql
+++ 3580-find-consistently-improving-employees/3580-find-consistently-improving-employees.sql
@@ -1,26 +1,28 @@
 # Write your MySQL query statement below
 -- LAST THREE REVIEW OF EACH EMPLOYEE 
-WITH last_3_review AS ( 
+WITH last_3_review AS (
     SELECT 
-    e.employee_id AS employee_id,
-    e.name AS name,
-    p.rating AS latest_rating,
-    Lead(rating) OVER (PARTITION BY e.employee_id ORDER BY p.review_date DESC) AS prev_rating1,
-    Lead(rating,2) OVER (PARTITION BY e.employee_id ORDER BY p.review_date DESC) AS prev_rating2,
-    ROW_NUMBER() OVER (PARTITION BY e.employee_id ORDER BY p.review_date DESC) AS rnk
-    FROM
-    employees e join performance_reviews p ON e.employee_id =p.employee_id
+        e.employee_id AS employee_id,
+        e.name AS name ,
+        r.rating as latest_rating,
+        LEAD(r.rating,1) OVER (PARTITION BY e.employee_id ORDER BY r.review_date DESC) AS prv1_rating,
+        LEAD(r.rating,2) OVER (PARTITION BY e.employee_id ORDER BY r.review_date DESC) AS prv2_rating,
+        ROW_NUMBER() OVER (PARTITION BY e.employee_id ORDER BY r.review_date DESC) AS rnk
+    FROM 
+        employees e 
+        JOIN performance_reviews r ON e.employee_id=r.employee_id
 )
 SELECT 
     employee_id,
     name,
-    (latest_rating - prev_rating2) AS improvement_score 
+    (latest_rating - prv2_rating) AS improvement_score
 FROM 
     last_3_review
 WHERE 
     rnk =1
-    AND prev_rating2 IS NOT NULL 
-    AND latest_rating > prev_rating1 
-    AND prev_rating1> prev_rating2
+    AND prv2_rating is not null
+    AND latest_rating > prv1_rating
+    AND prv1_rating > prv2_rating
 ORDER BY 
     improvement_score DESC ,name ASC;
+
