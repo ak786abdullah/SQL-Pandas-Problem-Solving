46a930ef6757022142c67f563e2ceb2f9ee3aaea
muhammad abdullah <abdullahkhosa11223344@gmail.com>
2026-07-13 23:59:57 -0700

    Delete Problem_24_Find_Customer_Referee directory

--- Problem_24_Find_Customer_Referee/Problem_24_Find_Customer_Referee.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def find_customer_referee(customer: pd.DataFrame) -> pd.DataFrame:
-    # Condition 1: referee_id is not 2
-    # Condition 2: referee_id is missing (NaN)
-    mask = (customer['referee_id'] != 2) | (customer['referee_id'].isna())
-    
-    # Apply the mask and return only the 'name' column
-    return customer[mask][['name']]
